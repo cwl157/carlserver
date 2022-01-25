8a2fe1699ae06f07e6aea65f810a8f26b51b835d
Carl <cwl157@gmail.com>
2022-01-25 14:59:52 -0600

    get blazor app up and running
    Add post page
    
    
    Add forward / back links between posts. Add disqus comments
    
    
    Add tags
    
    
    Add disclaimer, GA, and page titles
    
    
    Try out pre-render
    
    
    Add consent cookie. Fix post tag bug
    
    
    Add meta data tags
    
    
    Remove unused code
    
    
    Finish removing unused code
    
    
    Remove more unused code
    
    
    Update mini-bio
    
    
    Re-highlight and reload disqus when parameters change
    
    
    Add redirects
    
    
    Remove ads
    
    
    Update carlserver
    
    Update post body length of blazor site because no ads
    Change directory to save posts
    Remove old carlserver project

---
+++ src/carlserver/carlserver.blazor/Client/Helpers/StringEncoder.cs
@@ -0,0 +1,14 @@
+﻿namespace carlserver.blazor.Client.Helpers
+{
+    public static class StringEncoder
+    {
+      public static string EncodeString(string toEncode)
+        {
+            if (!string.IsNullOrEmpty(toEncode))
+            {
+                return Uri.EscapeDataString(toEncode);
+            }
+            return "";
+        }
+    }
+}
